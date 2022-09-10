1c5298da5cd225c25ff3615ec1791d283d56b044
gonemete <gonemete@gmail.com>
2022-09-10 10:20:47 +0200

    first commit

--- 05day_proyecto.py
+++ 05day_proyecto.py
@@ -4,20 +4,6 @@
 
 # Requisitos del proyecto
 # Añade un título con un print() para la pizzería, algo como -> Pizzería PF <-.
-# El usuario introducirá el dinero que quiera. Guárdalo en una variable.
-# Crea una lista donde ir añadiendo los ingredientes extra. Pista: métodos de adición en listas.
-# Habrá mínimo tres tipos de pizzas para elegir (pon las que quieras).
-# Cada pizza tendrá un coste diferente.
-# El usuario podrá elegir solo una pizza.
-# Una vez elegida la pizza, se le informará al usuario del total que lleva por el momento.
-# Se le debe solicitar, si quiere o no, añadir ingredientes extra (estos harán subir el precio final).
-# Añade al menos 3 ingredientes extra. El usuario podrá elegir ninguno, uno o varios de estos. No hay límite de ingredientes extra. Si se pasa del dinero que tiene, se le dirá que no le llega y que vuelva a realizar el pedido.
-# Las pizzas e ingredientes, tendrán sus precios almacenados en variables o constantes (piensa que los precios son los que son y no deben variar en todo el programa).
-# Con cada ingrediente extra, se le debe ir sumando el precio al total y mostrárselo al usuario con el cambio que le queda.
-# Si el usuario no quiere ingredientes extra, puede pagar directamente sin añadir ninguno.
-# Finalmente, se le debe presentar el ticket (imprimido en la consola) con el total gastado, el cambio y todos los elementos que se han añadido al pedido, pizza, ingredientes extra y precios.
-
-# Añade un título con un print() para la pizzería, algo como -> Pizzería PF <-.
 print("-> Pizzería PF <-")
 
 # El usuario introducirá el dinero que quiera. Guárdalo en una variable.
@@ -29,73 +15,52 @@ ingredientes_seleccionados=[]
 
 # Habrá mínimo tres tipos de pizzas para elegir (pon las que quieras).
 # Cada pizza tendrá un coste diferente.
+pizza_margarita=("Margarita", 10)
+pizza_mediterranea=("Medriterránea",14)
+pizza_cuatro_quesos=("4 Quesos", 16)
+print(f""" Lista de Precios
+    Pizza Margarita {pizza_margarita}€
+    Pizza Mediterránea {pizza_mediterranea}€
+    Pizza 4 Quesos {pizza_cuatro_quesos}€""")
+
+# El usuario podrá elegir solo una pizza.
+elige_pizza=int (input("Por favor elige una de las pizas (1-Margarita / 2-Mediterránea / 3-Cuatro Quesos): "))
+
+# Una vez elegida la pizza, se le informará al usuario del total que lleva por el momento.
+coste_actual=0
+if elige_pizza == 1:
+    coste_actual=pizza_margarita[1]
+    print(f"Llevas gastado {coste_actual}€")
+if elige_pizza == 2:
+    coste_actual=pizza_mediterranea[1]
+    print(f"Llevas gastado {coste_actual}€")
+if elige_pizza == 3:
+    coste_actual=pizza_cuatro_quesos[1]
+    print(f"Llevas gastado {coste_actual}€")
+
+# Se le debe solicitar, si quiere o no, añadir ingredientes extra (estos harán subir el precio final).
+anadir_extras=False
+pregunta_extras=input("Deseas añadir ingredientes extras? ")
+if pregunta_extras.lower() == "si":
+    anadir_extras=True
+if pregunta_extras.lower() == "no":
+    anadir_extras=False
+
+if anadir_extras==False:
+    print(f"El precio de su pedido es de: {coste_actual}")    
+# Añade al menos 3 ingredientes extra. El usuario podrá elegir ninguno, uno o varios de estos. No hay límite de ingredientes extra. Si se pasa del dinero que tiene, se le dirá que no le llega y que vuelva a realizar el pedido.
+# Las pizzas e ingredientes, tendrán sus precios almacenados en variables o constantes (piensa que los precios son los que son y no deben variar en todo el programa).
+# Con cada ingrediente extra, se le debe ir sumando el precio al total y mostrárselo al usuario con el cambio que le queda.
+# Si el usuario no quiere ingredientes extra, puede pagar directamente sin añadir ninguno.
+# Finalmente, se le debe presentar el ticket (imprimido en la consola) con el total gastado, el cambio y todos los elementos que se han añadido al pedido, pizza, ingredientes extra y precios.
+
+
+
+
+# Habrá mínimo tres tipos de pizzas para elegir (pon las que quieras).
+# Cada pizza tendrá un coste diferente.
 # El usuario podrá elegir solo una pizza.
 #Esto lo hago así con variables para el precio y el nombre según se eliga pero lo podría haber hecho con una tupla:
 #y quedaría así  pizza_margarita=("Margariata", 10)
-pizza_margarita=10
-pizza_mediterranea=14
-pizza_cuatro_quesos=16
 
-print(f"""
-Bienvenido!
-Tenemos las siguientes Pizzas a elegir (Sólo una):
-- Pizza Margarita ........ {pizza_margarita}€
-- Pizza Mediterránea ..... {pizza_mediterranea}€
-- Pizza 4 Quesos ......... {pizza_cuatro_quesos}€
-""")
-eleccion=int(input("""
-Por favor eliga una:
-1 - Pizza Margarita
-2 - Pizza Mediterránea
-3 - Pizza 4 Quesos\n
-"""))
-elif eleccion<0 or eleccion>4:
-    eleccion=int(input("""
-Por favor eliga una (marque del 1 al 3):
-1 - Pizza Margarita
-2 - Pizza Mediterránea
-3 - Pizza 4 Quesos\n
-"""))
 
-if eleccion == 1:
-    print(f"Ha elegido Pizza Margarita\n")
-    saldo_actual=saldo_usuario-pizza_margarita
-    print(f"Su saldo actual es de: {saldo_actual}€")
-elif saldo_actual<0:
-    print(f"Su saldo: {saldo_actual}")
-    print("No tiene suficiente dinero por favor vuelva a elegir")
-    eleccion=int(input("""
-Por favor eliga una (marque del 1 al 3):
-1 - Pizza Margarita
-2 - Pizza Mediterránea
-3 - Pizza 4 Quesos\n
-"""))
-        # break
-if eleccion == 2:
-    print(f"Ha elegido Pizza Mediterránea\n")
-    saldo_actual=saldo_usuario-pizza_mediterranea
-    print(f"Su saldo actual es de: {saldo_actual}€")
-elif saldo_actual<0:
-print(f"Su saldo: {saldo_actual}")
-print("No tiene suficiente dinero por favor vuelva a elegir")
-eleccion=int(input("""
-Por favor eliga una (marque del 1 al 3):
-1 - Pizza Margarita
-2 - Pizza Mediterránea
-3 - Pizza 4 Quesos\n
-"""))
-        # break
-else:
-    print(f"Ha elegido Pizza 4 Quesos\n")
-    saldo_actual=saldo_usuario-pizza_cuatro_quesos
-    print(f"Su saldo actual es de: {saldo_actual}€")
-    while saldo_actual<0:
-        print(f"Su saldo: {saldo_actual}")
-        print("No tiene suficiente dinero por favor vuelva a elegir")
-        eleccion=int(input("""
-Por favor eliga una (marque del 1 al 3):
-1 - Pizza Margarita
-2 - Pizza Mediterránea
-3 - Pizza 4 Quesos\n
-"""))
-        # break
